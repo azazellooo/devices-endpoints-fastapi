1f6b822e02cba9bf9036dab6f1f0da66aef12c81
karlygach <karygachkenenbaeva@gmail.com>
2021-11-13 14:53:15 +0600

    added database initialization

---
+++ db.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+import os
+
+engine = create_engine(
+    f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@localhost/{os.environ.get('DB_NAME')}",
+    echo=True
+)
+
+Base = declarative_base()
+SessionLocal = sessionmaker(bind=engine)
